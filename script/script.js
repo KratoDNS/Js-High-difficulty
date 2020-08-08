276d219f79de8e665b8ecab19e90521de4ee4d77
Gennady Systerov <gena.systerov@yandex.ru>
2020-08-08 05:15:15 +0400

    закончил задачи

--- script/script.js
+++ script/script.js
@@ -2,3 +2,9 @@ let num=266219
 let str= String(num)
 console.log(str)
 console.log(str.split('',6))
+let multiply = str[1] * str[2] *str[3] * str[4] * str[5] * str[0]
+console.log(multiply)
+console.log(typeof multiply)
+console.log(multiply**3)
+let final=String(multiply**3)
+console.log(final.split('',2))
