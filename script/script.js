1b7999a6866e2daccdd861ae6bf6d213f09c0175
Gennady Systerov <gena.systerov@yandex.ru>
2020-08-12 06:54:13 +0400

    вывел ruweek

--- script/script.js
+++ script/script.js
@@ -1,7 +1,13 @@
-let lang = 'ru' || 'en'
+let lang = prompt('Введите ru или en')
+let ru=  1
+let en = 1
+
+
+
 let ruweek='понедельник, вторник, среда, четверг, пятница, суббота, воскресенье'
 let enweek='monday, tuesday, Wednesday, Thursday, friday, Saturday, sunday'
-prompt(lang)
-if (lang = 'ru'){
-    console.log(ruweek.split(' ', 7) )
-}   else (console.log(enweek.split(' ', 7)))
+    if (lang = ru){
+    alert(ruweek.split(' ', 7))
+}   else {
+    alert(enweek.split(' ', 7))
+}
